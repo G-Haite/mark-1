daa2a85a95b1733dd609502a113ee43a5b112046
Gusty <gustavohaite@gmail.com>
2020-10-27 16:35:31 -0300

    actualizacion del index y de la pestaña de ayuda. Ademas se cambio la letra del nav

--- prueba.js
+++ prueba.js
@@ -1,5 +1,14 @@
-var register = false;
-var username = Gusty;
-var lastname = Haite;
+var register ={
+    userName: 'Gustavo',
+    lastName: 'Haite',
+    isIt: false
+}
 
+function changeTitle(){
+    var title = document.querySelector("#mainTitle");
+    title.style.color = 'black';
+    title.style.background = "yellow";
+    title.style.border = "5px dashed red"
+    title.innerHTML = "aguante boquita"
+}
 
